7c5ce7eeed851e88ca448dfe5e3457a74709e243
Marysia Pabich <marysia.pabich@gmail.com>
2020-02-12 16:39:51 +0100

    Display start button only if app status is off

--- app/script.js
+++ app/script.js
@@ -18,6 +18,7 @@ class App extends React.Component {
         <section>
           <p>According to optometrists in order to save your eyes, you should follow the 20/20/20. It means you should to rest your eyes every 20 minutes for 20 seconds by looking more than 20 feet away.</p>
           <p>This app will help you track your time and inform you when it's time to rest.</p>
+          <button className="btn">Start</button>
         </section>
         ): ''}
         
@@ -36,9 +37,6 @@ class App extends React.Component {
             <button className="btn">Stop</button>
            </section>
          ) : ''} 
-
-        <button className="btn">Start</button>
-
         <button className="btn btn-close">X</button>
       </div>
     )
