8e8a4c075dbb43c9e0322b5083eaa1efebc8ff10
Marysia Pabich <marysia.pabich@gmail.com>
2020-02-13 18:20:54 +0100

    Change app status, set interval & set 20 minutes on Start click

--- app/script.js
+++ app/script.js
@@ -18,6 +18,18 @@ class App extends React.Component {
     return output;
   }
 
+  step () {
+    console.log('step ON')
+  }
+
+  startTimer = () => {
+    this.setState({
+      status: 'work',
+      time: 1200,
+      timer: setInterval(() => this.step(), 1000)
+    });
+  }
+
   render() {
     return (
       <div>
@@ -26,7 +38,7 @@ class App extends React.Component {
         <section>
           <p>According to optometrists in order to save your eyes, you should follow the 20/20/20. It means you should to rest your eyes every 20 minutes for 20 seconds by looking more than 20 feet away.</p>
           <p>This app will help you track your time and inform you when it's time to rest.</p>
-          <button className="btn">Start</button>
+          <button className="btn" onClick={this.startTimer}>Start</button>
         </section>
         ): ''}
         
